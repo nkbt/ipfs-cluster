7df4b5a2272fb29f9848a4c88b7578682c1530f7
Nikita Butenko <nikita@butenko.au>
2024-04-11 14:38:04 +1000

    Use swarm key for 3 out of 7 ipfs nodes

--- ipfs-bootstrap.sh
+++ ipfs-bootstrap.sh
@@ -5,3 +5,5 @@ ipfs bootstrap rm --all
 
 # Add private peers for quicker bootstrap if necessary
 # ipfs bootstrap add "/ip4/$PRIVATE_PEER_IP_ADDR/tcp/4001/ipfs/$PRIVATE_PEER_ID"
+
+ipfs config --json Routing.Type '"dht"'
